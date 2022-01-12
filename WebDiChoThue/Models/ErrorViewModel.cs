e1b86aa4dd06e337c7b247fb54b26cc3404f4292
SƠN VÕ NGUYỄN HỒNG <18120538@student.hcmus.edu.vn>
2022-01-12 21:03:03 +0700

    abc

---
+++ WebDiChoThue/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebDiChoThue.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
